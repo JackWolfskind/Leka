339dd1f68f50a95aff2e82c8132e9d7f284e9fa3
Christian Ziermann <cziermann@mailbox.org>
2017-11-07 10:15:07 +0100

    init Simple php framework, remove Symfony

--- web/src/public/index.php
+++ web/src/public/index.php
@@ -1,16 +1,30 @@
 <?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpKernel;
+require __DIR__ . '/../../vendor/autoload.php';
 
-require __DIR__.'/../../bootstrap.php';
+session_start();
 
-$kernel = $container[HttpKernel::class];
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// Instantiate the app
+$settings = require __DIR__ . '/../php/settings.php';
+$app = new \Slim\App($settings);
 
-use Nstrohe\Klarheit\Lib\Container;
-$container = new Container(__DIR__ . '/../../../app-config/globals.php');
-define('BASE_PATH', $container['path']['base']);
+// Set up dependencies
+require __DIR__ . '/../php/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../php/middleware.php';
+
+// Register routes
+require __DIR__ . '/../php/routes.php';
+
+// Run app
+$app->run();

8c2347e72014b8aad015f49ea7af938dffa69f09
Chase <dies200@rocketmail.com>
2017-11-07 22:53:43 +0100

    added nanoHTTPD and implemented HTTPServer

--- java/src/main/java/org/gso/leka/Main.java
+++ java/src/main/java/org/gso/leka/Main.java
@@ -1,9 +1,19 @@
 package org.gso.leka;
 
+import java.io.IOException;
+
 import javax.naming.NamingException;
+
+import org.gso.leka.http.HttpServer;
 
 public class Main {
 	public static void main(String[] args) throws NamingException {
-		new LDAPHelper();
+		//new LDAPHelper();
+		try {
+			new HttpServer(8080).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }

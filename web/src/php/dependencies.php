a26e3ddcf40d7f5f565fa1ee04ad77b55d2c0677
Christian Ziermann <cziermann@mailbox.org>
2017-11-07 15:07:46 +0100

    add twig rendering

--- web/src/php/dependencies.php
+++ web/src/php/dependencies.php
@@ -1,12 +1,21 @@
 <?php
+
 // DIC configuration
 
 $container = $app->getContainer();
 
 // view renderer
 $container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+        $settings = $c->get('settings')['renderer'];
+        $view = new \Slim\Views\Twig($settings['template_path'], [
+        'cache' => $settings['cache_path']
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
 };
 
 // monolog

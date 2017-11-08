f1aa96e6f1a58c88ce59e0be2f30bc2c21baa465
Lukas Prediger <lukas@predigerweb.de>
2017-11-08 13:56:56 +0000

    More Entitys

--- java/src/main/java/org/gso/leka/data/Block.java
+++ java/src/main/java/org/gso/leka/data/Block.java
@@ -1,5 +1,26 @@
 package org.gso.leka.data;
 
-public class Block {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "adm_block")
+public class Block {
+	
+	@Id
+	@Column(name="adm_st_id")
+	private int ID;
+	
+	@Column(name="adm_st_name")
+	private String name;
+	
+	@Column(name="adm_st_anfang")
+	private Date start;
+	
+	@Column(name="adm_st_ende")
+	private Date end;
 }
--- java/src/main/java/org/gso/leka/data/SchoolClass.java
+++ java/src/main/java/org/gso/leka/data/SchoolClass.java
@@ -1,5 +1,8 @@
 package org.gso.leka.data;
 
+import java.util.List;
+
 public class SchoolClass {
 	private String ID;
+	private List<Student> students;
 }

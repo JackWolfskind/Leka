8a106015439fa1ed210fc0ce49800cf9acb0a4ea
Christian Ziermann <cziermann@mailbox.org>
2017-11-09 13:15:02 +0100

    move auth to controller

--- web/src/php/Controller/AuthController.php
+++ web/src/php/Controller/AuthController.php
@@ -20,15 +20,20 @@ class AuthController extends BaseControlller {
 
     public function login(Response $request, Request $response, $args) {
         // Render index view
-        $password = $request->getAttribute("password");
-        $username = $request->getAttribute("username");
-
+        $password = $request->getParam("password");
+        $username = $request->getParam("username");
+        #$newPass = iconv( 'UTF-8', 'UTF-16LE', $password );
         $ldapconn = ldap_connect("ldap-dc1.gso") or die("Could not connect to LDAP server.");
         ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        $ldapbind = ldap_bind($ldapconn, "gso\\" . $username, $password) or die("anonymes LDAP bind fehlgeschlagen...");
-        $ldap_search = ldap_search($ldapconn, "ou=benutzer,dc=gso-neu", "uid=" . $username);
-        $info = ldap_get_entries($ldapconn, $ldap_search);
-        echo "Daten für " . $info["count"] . " Items gefunden:<br />";
+        ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+        $bind = @ldap_bind($ldapconn);
+        $res_id = ldap_search($ldapconn, "ou=fia51,ou=it,ou=schueler,ou=benutzer,dc=gso-neu", "uid=$username");
+        $entry_id = ldap_first_entry($ldapconn, $res_id);
+        $user_dn = ldap_get_dn($ldapconn, $entry_id);
+        $link_id = ldap_bind($ldapconn, $user_dn, $password);
+        if ($link_id) {
+            $_SESSION["teacher"] = $username;
+        }
     }
 
     public function logout(Response $request, Request $response, $args) {

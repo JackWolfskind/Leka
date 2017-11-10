d5ea4e6c69e948784c4a6007aa4c959b354508a3
Lukas Prediger <lukas@predigerweb.de>
2017-11-10 12:20:41 +0000

    corrected http api

--- java/src/main/java/org/gso/leka/data/block/BlockReadHandler.java
+++ java/src/main/java/org/gso/leka/data/block/BlockReadHandler.java
@@ -1,13 +1,9 @@
 package org.gso.leka.data.block;
 
-import java.util.Date;
 import java.util.Map;
-
-import org.gso.leka.Main;
 import org.gso.leka.http.HttpHandler;
 import org.gso.leka.http.HttpServer;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
@@ -15,22 +11,18 @@ import fi.iki.elonen.NanoHTTPD.Response;
 import fi.iki.elonen.NanoHTTPD.Response.Status;
 
 
-public class GradeListHandler extends HttpHandler {
+public class BlockReadHandler extends HttpHandler {
 	
-	public GradeListHandler() {
-		super("list");
+	public BlockReadHandler() {
+		super("read");
 	}
 
 	@Override
 	public Response Handle(IHTTPSession session, String route) {
 		Map<String, JsonElement> parameters = parseParameters(session.getInputStream());
-		Response reply = HttpServer.newFixedLengthResponse(
-				new Gson().toJson(Block.loadAll(Main.entityManagerFactory.createEntityManager(),
-						new Date(parameters.get("start").getAsLong()),
-						new Date(parameters.get("end").getAsLong()),
-						parameters.get("name").getAsString())));
-		reply.setStatus(Status.OK);
+		Response reply = HttpServer.newFixedLengthResponse(Status.NOT_IMPLEMENTED.getDescription());
 		
+		reply.setStatus(Status.NOT_IMPLEMENTED);
 		return reply;
 	}
 
